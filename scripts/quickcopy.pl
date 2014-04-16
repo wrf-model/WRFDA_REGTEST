618bdf231d26448c117291413d2dae125d579c31
Michael Kavulich <kavulich@ucar.edu>
2014-04-16 22:41:06 +0000

    Cleaned up quickcopy script
    git-svn-id: https://svn-user-kavulich.cgd.ucar.edu/trunk/REGTEST@40 95a97b06-1a33-401f-800d-6477af4236fe

--- scripts/quickcopy.pl
+++ scripts/quickcopy.pl
@@ -1,22 +1,25 @@
 #!/usr/bin/perl -w
 # Created March 27, 2013, Mike Kavulich. 
+# Edited  April 03, 2014, Mike Kavulich: unchanged files as list rather than messy output, allow baseline name as input
 # No rights reserved, user is free to modify and distribute as they wish.
 use File::Compare;
 use File::Copy;
 
 my $checkall = "";
+my @changed;
+my @unchanged;
+my $uncopied_num = 0; #Note: uncopied_num will actually be the size of @unchanged, but @unchanged is indexed from 0
+
 foreach $arg (@ARGV) {
     $checkall = "$checkall $arg" ;
 }
 
 my $q = (@ARGV);
 
-
 my @files = `find . -maxdepth 2 -follow -name "wrfvar_output.*"`;
 
-my $loopnum = 0;
+my $copied_num = 0; #Note: copied_num will actually be the size of @changed, but @changed is indexed from 0
 foreach $filename (@files) {
-    $loopnum ++;
     chomp $filename;
     next if ($filename =~ m/BASELINE/i);
 
@@ -25,13 +28,35 @@ foreach $filename (@files) {
     next if ( ($checkall !~ m/$fileparts[1]\s/i) && ($q > 0) );
 
     unless ( compare ("$filename","BASELINE.NEW/$fileparts[2]") ) {
-       print "\n  'BASELINE.NEW/$fileparts[2]' AND '$filename' ARE EQUAL, DOING NOTHING.\n";
+       $unchanged[$uncopied_num] = $filename;
+       $uncopied_num = @unchanged;
        next ;
     }
     mkdir "BASELINE.NEW.BACKUP" unless (-e "BASELINE.NEW.BACKUP");
+    unless (-e "BASELINE.NEW/$fileparts[2]") {
+        # If there is no baseline file, don't copy. For new tests you should add the baseline manually.
+        print "WARNING: $filename does not have a baseline file for comparison! Not copying.\n";
+    }
     copy ("BASELINE.NEW/$fileparts[2]","BASELINE.NEW.BACKUP") or die "Cannot copy 'BASELINE.NEW/$fileparts[2]' to 'BASELINE.NEW.BACKUP': $!";
-    copy ("$filename","BASELINE.NEW") or die "Cannot copy '$filename' to 'BASELINE.NEW': $_";
+    copy ("$filename","BASELINE.NEW") or die "Cannot copy '$filename' to 'BASELINE.NEW': $!";
     print "'$filename' copied to BASELINE.NEW, old baseline file backed up to BASELINE.NEW.BACKUP\n";
+    $copied_num ++;
+}
 
+if ($copied_num > 0){
+    print "The following $copied_num files were copied to BASELINE.NEW, old baseline file were backed up to BASELINE.NEW.BACKUP:\n\n";
+    foreach $changed_name (@changed) {
+        print "$changed_name\n";
+    }
+
+
+    unless ($uncopied_num < 1) {
+        print "The following $uncopied_num files were bit-for-bit identical to the baseline in BASELINE.NEW, and were not copied:\n\n";
+        foreach $unchanged_name (@unchanged) {
+            print "$unchanged_name\n";
+        }
+    }
+} else {
+    print "All $uncopied_num files were bit-for-bit identical to the baseline in BASELINE.NEW, and were not copied.\n";
 }
 
