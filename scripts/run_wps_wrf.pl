b6a41b39fee3559b286b751f3d53f48d3aaa8d8c
mkavulich <kavulich@ucar.edu>
2016-09-27 14:20:12 -0600

    Major updates to script for running WPS and WRF for GEN_BE. Changes are in order of appearance, not importance:
     - Allow reading of GRIB files from multiple directories (multiple months)
     - Allow WPS input data to be from FNL or NAM
     - Allow specification of NUM_METGRID_LEVELS and NUM_METGRID_SOIL_LEVELS, since these change over time and between models
     - Allow specification of different Vtables
     - Various comments for clarification
     - Adding new script variables for namelist: PARENT_TIME_STEP_RATIO, BL_PBL_PHYSICS, CU_PHYSICS, and PTOP
     - Move subroutines to bottom
     - Add NOCOLONS option to namelist (commented for now)
     - Change all "FNL" names to generic "GRIB" names, since other data is now supported
     - Make "getgribfiles" subroutine, to split out that functionality

--- scripts/run_wps_wrf.pl
+++ scripts/run_wps_wrf.pl
@@ -31,36 +31,49 @@ use Getopt::Long;
  print "Start time: $Start_time\n";
 
 # Basic inputs
- my $Main_dir = "/glade/p/work/kavulich/V37";
+ my $Main_dir = "/glade/p/work/kavulich/V38";
  my $Script_dir = `pwd`;
  chomp $Script_dir;
  my $WRF_dir="$Main_dir/WRFV3";
  my $WPS_dir="$Main_dir/WPS";
- my $GRIB_dir="/glade/p/rda/data/ds083.2/grib2/2008/2008.02";   #Where to find grib input data
+ my @GRIB_dir;
+# $GRIB_dir[0]="/glade/p/rda/data/ds609.0/2015/";   #Where to find grib input data
+#If your forecasts span multiple months, put the subsequent directory names in subsequent array values. Should work for indefinitely long runs
+ $GRIB_dir[0]="/glade/p/rda/data/ds083.2/grib2/2016/2016.05";
+ $GRIB_dir[1]="/glade/p/rda/data/ds083.2/grib2/2015/2015.12";
+ my $Data_type = "FNL"; # Valid choices: FNL, NAM
+
  my $geog_dir="/glade/p/work/wrfhelp/WPS_GEOG/";
+ my $alt_Vtable=""; #Set this if you are not using GFS/FNL data, or want to do something strange
+# my $alt_Vtable="$WPS_dir/ungrib/Variable_Tables/Vtable.GFS";
+# my $alt_Vtable="/glade/p/work/kavulich/V371/WPS/ungrib/Variable_Tables/Vtable.GFS";
  my $WORKDIR="/glade/scratch/kavulich/GEN_BE_FORECASTS";
 
 # Directories for running WPS/WRF and storing output
- my $Exp_name="tutorial_CONUS60_genbe_monthlong";
- my $Run_dir="$WORKDIR/$Exp_name";
- my $Out_dir="$Script_dir/Output/$Exp_name";
+ my $Case_name="hawaii_gpsztd";
+ my $Run_dir="$WORKDIR/Run/$Case_name";
+ my $Out_dir="$WORKDIR/Output/$Case_name";
 
 # Start and end dates
  my $initial_date="2008-02-01_00:00:00"; #Initial time for first forecast
  my $final_date="2008-03-01_00:00:00";   #Initial time for final forecast
 
 # Set number of WPS time stamps for each cycle
- my $WPS_times=5;
+ my $WPS_times=3;
 
-# WRF parameters
+# real.exe parameters
+ my $NUM_METGRID_LEVELS=32; #GFS Default 27 (upped to 32 in April 2016); NAM default 40
+ my $NUM_METGRID_SOIL_LEVELS=4; #GFS Default "4"; prior to early 2005 it was "2"
+
+# WRF/WPS parameters
  my $METEM_INTERVAL=21600;   #WPS output interval               IN SECONDS
  my $OUT_INTERVAL=720;       #WRF output interval               IN MINUTES
  my $FC_INTERVAL=720;        #Interval between forecasts        IN MINUTES
  my $GRIB_INTERVAL=6;        #WPS GRIB input interval           IN HOURS
                              # I'm so, so sorry about this part ^^^^^^^^^^ 
                              # but it's necessary due to WPS/WRF namelist conventions
- my $RUN_DAYS = 0;
- my $RUN_HOURS = 24;
+ my $RUN_DAYS = 0;           # Don't make this a month or longer or bad things will happen!
+ my $RUN_HOURS = 6;
  my $RUN_MINUTES = 0;
  my $NUM_DOMAINS = 1; #For NUM_DOMAINS > 1, be sure that the appropriate variables are all set for all domains below!
  my $WRF_DT = 360;
@@ -73,7 +86,7 @@ use Getopt::Long;
  my @J_PARENT_START = ( 1, 125 );
 # my $NL_ETA_LEVELS="1.000 0.9880 0.9765 0.9620 0.9440 0.9215 0.8945 0.8587 0.8161 0.7735 0.7309 0.6724 0.6010 0.5358 0.4763 0.4222 0.3730 0.3283 0.2878 0.2512 0.2182 0.1885 0.1619 0.1380 0.1166 0.0977 0.0808 0.0659 0.0528 0.0412 0.0312 0.0224 0.0148 0.0083 0.0026 0.0000";
 # my $NL_ETA_LEVELS="1.000000,0.998000,0.996000,0.994000,0.992000,0.990000,0.988100,0.981800,0.974000,0.966000,0.958000,0.952000,0.943400,0.920000,0.880000,0.840000,0.800000,0.760000,0.720000,0.680000,0.640000,0.600000,0.560000,0.520000,0.480000,0.440000,0.400000,0.360000,0.320000,0.280000,0.240000,0.200000,0.160000,0.140000,0.120000,0.100000,0.080000,0.060000,0.040000,0.020000,0.00000";
- my $MAP_PROJ="lambert";
+ my $MAP_PROJ="lambert"; #"lambert", "polar", etc.
  my $REF_LAT=40.00001;    #AKA PHIC AKA CEN_LAT
  my $REF_LON=-95.;     #AKA XLONC AKA CEN_LON
  my $STAND_LON=-95.;
@@ -83,12 +96,16 @@ use Getopt::Long;
  my $POLE_LON=0.;
 
 # WRF options
+ my @PARENT_TIME_STEP_RATIO = ( 1, 3 );
  my @MP_PHYSICS = ( 3, 4 );
  my @RA_LW_PHYSICS = ( 1, 24 );
  my @RA_SW_PHYSICS = ( 1, 24 );
  my $RADT = 60;
  my @SF_SFCLAY_PHYSICS = ( 1, 1);
  my @SF_SURFACE_PHYSICS = ( 1, 2);
+ my @BL_PBL_PHYSICS = ( 1, 1);
+ my @CU_PHYSICS = ( 1, 1);
+ my $PTOP = 3000;
 
 # Job submission options
  my $MAX_JOBS = 50;
@@ -112,7 +129,7 @@ use Getopt::Long;
 # If old data exists, ask to overwrite
  if (-d $Out_dir) {
     my $go_on='';
-    print "$Out_dir already exists, do you want to overwrite?\a\n";
+    print "$Out_dir already exists, do you want to risk overwriting old data?\a\n";
     while ($go_on eq "") {
        $go_on = <STDIN>;
        chop($go_on);
@@ -136,43 +153,9 @@ use Getopt::Long;
  my $job_feedback; #For getting feedback from bsub
  my $jobid;        #For making sure we submit jobs in the right order
 
-#Need a subroutine to convert WRF-format dates to numbers for comparison
- sub wrf2num {
-    my ($wrf_date) = @_;
-    $wrf_date =~ s/\D//g;
-    return $wrf_date;
- }
 
- sub current_jobs {
-    my $bjobs = `bjobs`;
-    my $jobnum = $bjobs =~ tr/\n//;
-#    print "Currently $jobnum jobs are pending or running\n";
-    return $jobnum;
- }
-
- sub run_checks { #Subroutine to check for failures and limit simultaneous jobs
-    my $num_jobs =  &current_jobs;
-    if ($num_jobs > $MAX_JOBS) {
-       print "\n\n*********\n* PAUSE *\n*********\n";
-       print "MAX_JOBS exceeded, waiting before submitting next forecast.\n";
-       printf "We have been waiting for   0 minutes...";
-       my $wait_time = 0;
-       while ($num_jobs > $MAX_JOBS){
-          sleep(60);
-          $wait_time ++;
-          printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b%03d minutes...",$wait_time;
-          $num_jobs =  &current_jobs;
-       }
-    }
-    if (-e "$Script_dir/FAIL") {
-       die "\nFailure reported! See file '$Script_dir/FAIL' for details\n";
-    }
- }
-
- sub watch_progress { # When waiting for jobs to finish, let's display some info
-
-
- }
+ my $monthnum = 0; #For multi-month runs, keep track of which month we're in
+ my $straddle = 0; #For forecasts which straddle two months, we'll set this flag. You'll see why below
 
  while ( &wrf2num($fcst_date) <= &wrf2num($final_date) ) {
     &run_checks; #Run failure and job number checks before starting new forecast
@@ -200,7 +183,7 @@ use Getopt::Long;
     my $emin   = substr("$fcst_end", 14, 2);
     my $esec   = substr("$fcst_end", 17);
 
-    print "Setting up working directory for forecast\n";
+    print "Setting up working directory for forecast:\n$Run_dir/$fcst_date\n\n";
 
     my $fcst_dirname = substr(&wrf2num($fcst_date),0,10);
 
@@ -209,17 +192,32 @@ use Getopt::Long;
 
     # Get WPS files
     ! system("cp $WPS_dir/link_grib.csh $WPS_dir/geogrid/src/geogrid.exe $WPS_dir/ungrib/src/ungrib.exe $WPS_dir/metgrid/src/metgrid.exe $Run_dir/$fcst_date/") or die "Error copying WPS files: $!\n";
-    copy ("$WPS_dir/ungrib/Variable_Tables/Vtable.GFS","$Run_dir/$fcst_date/Vtable");
 
-    # Get WRF files; use glob since we need everything
-    my @WRF_files = glob("$WRF_dir/run/*");
-    copy ("$WRF_dir/run/*","$Run_dir/$fcst_date/");
+    if ($alt_Vtable ne "") {
+       print "Using alternate Vtable: $alt_Vtable\n";
+       copy ("$alt_Vtable","$Run_dir/$fcst_date/Vtable");
+    } else {
+       if ($Data_type eq "FNL") {
+          print "Using standard $Data_type Vtable: $WPS_dir/ungrib/Variable_Tables/Vtable.GFS\n";
+          copy ("$WPS_dir/ungrib/Variable_Tables/Vtable.GFS","$Run_dir/$fcst_date/Vtable");
+       } elsif ($Data_type eq "NAM") {
+          print "Using standard $Data_type Vtable: $WPS_dir/ungrib/Variable_Tables/Vtable.NAM\n";
+          copy ("$WPS_dir/ungrib/Variable_Tables/Vtable.NAM","$Run_dir/$fcst_date/Vtable");
+       }
+    }
 
-    foreach my $file (@WRF_files) {
-       copy("$file","$Run_dir/$fcst_date/");
+    unless ($run_wrf == 0) {
+       # Get WRF files; use glob since we need everything
+       my @WRF_files = glob("$WRF_dir/run/*");
+       copy ("$WRF_dir/run/*","$Run_dir/$fcst_date/");
+
+       foreach my $file (@WRF_files) {
+          copy("$file","$Run_dir/$fcst_date/");
+       }
     }
 
     chdir "$Run_dir/$fcst_date";
+    unlink "namelist.wps";
     unlink "namelist.input";
     chmod 0755, "real.exe" or die "Couldn't change the permission to real.exe: $!";
     chmod 0755, "wrf.exe" or die "Couldn't change the permission to wrf.exe: $!";
@@ -237,6 +235,7 @@ use Getopt::Long;
     print NL " interval_seconds = $METEM_INTERVAL\n";
     print NL " io_form_geogrid = 2,\n";
     print NL " debug_level = 0\n";
+#    print NL " nocolons                 = true,\n";
     print NL "/\n";
     print NL "&geogrid\n";
     print NL " parent_id         =   1,   1,\n";
@@ -270,7 +269,6 @@ use Getopt::Long;
 
 # WRF NAMELIST
     open NL, ">namelist.input" or die "Can not open namelist.input for writing: $! \n";
-
     print NL "&time_control\n";
     print NL " run_days                 = $RUN_DAYS,\n";
     print NL " run_hours                = $RUN_HOURS,\n";
@@ -295,10 +293,12 @@ use Getopt::Long;
     print NL " restart                  = .false.,\n";
     print NL " restart_interval         = 500000,\n";
     print NL " debug_level              = 0,\n";
+#    print NL " nocolons                 = true,\n";
     print NL "/\n";
     print NL "&domains\n";
     print NL " time_step                = $WRF_DT,\n";
     print NL " max_dom                  = $NUM_DOMAINS,\n";
+    print NL " parent_time_step_ratio   = $PARENT_TIME_STEP_RATIO[0], $PARENT_TIME_STEP_RATIO[1],\n";
     print NL " e_we                     = $WEST_EAST_GRID[0], $WEST_EAST_GRID[1],\n";
     print NL " e_sn                     = $SOUTH_NORTH_GRID[0], $SOUTH_NORTH_GRID[1],\n";
     print NL " e_vert                   = $VERTICAL_GRID[0], $VERTICAL_GRID[1],\n";
@@ -306,9 +306,9 @@ use Getopt::Long;
     print NL " dy                       = $WRF_DX[0],$WRF_DX[1],\n";
 #    print NL " eta_levels               = $NL_ETA_LEVELS\n";
     print NL " smooth_option            = 1,\n";
-    print NL " p_top_requested          = 5000,\n";
-    print NL " num_metgrid_levels       = 27,\n";
-    print NL " num_metgrid_soil_levels  = 4,\n";
+    print NL " p_top_requested          = $PTOP\n";
+    print NL " num_metgrid_levels       = $NUM_METGRID_LEVELS,\n";
+    print NL " num_metgrid_soil_levels  = $NUM_METGRID_SOIL_LEVELS,\n";
     print NL " grid_id                  = 1, 2,\n";
     print NL " parent_id                = 0, 1,\n";
     print NL " i_parent_start           = $I_PARENT_START[0],  $I_PARENT_START[1],\n";
@@ -324,9 +324,9 @@ use Getopt::Long;
     print NL " radt                     = $RADT, $RADT,\n";
     print NL " sf_sfclay_physics        = $SF_SFCLAY_PHYSICS[0], $SF_SFCLAY_PHYSICS[1],\n";
     print NL " sf_surface_physics       = $SF_SURFACE_PHYSICS[0], $SF_SURFACE_PHYSICS[1],\n";
-    print NL " bl_pbl_physics           = 1,\n";
+    print NL " bl_pbl_physics           = $BL_PBL_PHYSICS[0], $BL_PBL_PHYSICS[1],\n";
     print NL " bldt                     = 0,\n";
-    print NL " cu_physics               = 1,\n";
+    print NL " cu_physics               = $CU_PHYSICS[0], $CU_PHYSICS[1],\n";
     print NL " cudt                     = 5,\n";
     print NL " isfflx                   = 1,\n";
     print NL " ifsnow                   = 1,\n";
@@ -364,24 +364,23 @@ use Getopt::Long;
     print NL "/\n";
     close NL;
 
-    #Link FNL GRIB data
+
+    #Link GRIB data
     print "Linking input data\n";
 
-    my $fnl_date = $fcst_date;
-    while ( &wrf2num($fnl_date) <= &wrf2num($fcst_end) ) {
-       my $fnlyear  = substr("$fnl_date", 0, 4);
-       my $fnlmonth = substr("$fnl_date", 5, 2);
-       my $fnlday   = substr("$fnl_date", 8, 2);
-       my $fnlhour  = substr("$fnl_date", 11, 2);
-       
-       my @fnl_file = glob("$GRIB_dir/fnl_$fnlyear$fnlmonth$fnlday\_$fnlhour*");
-       symlink $fnl_file[0], "fnl_$fnlyear$fnlmonth$fnlday\_$fnlhour" or warn "Cannot symlink $_ to local directory: $!\n";
-
-       $fnl_date = `$Script_dir/da_advance_time.exe $fnl_date ${GRIB_INTERVAL}h -w`;
-       chomp ($fnl_date);
+    my $grib_date = $fcst_date;
+    if ($smonth == $emonth) {
+       if ($straddle == 1) { #If start month and end month are the same and straddle == 1, that means we've fully entered the next month
+          $monthnum ++;
+          $straddle = 0;
+       }
+    } else {
+       $straddle = 1;
     }
 
-# RUN WPS
+    &getgribfiles($grib_date, $fcst_end, $smonth );
+
+   # RUN WPS
 
     print "Creating WPS job\n";
 
@@ -399,9 +398,11 @@ use Getopt::Long;
     print FH "#BSUB -P $PROJECT\n";
     printf FH "#BSUB -R span[ptile=1]\n";
     print FH "\n"; #End of BSUB commands; add newline for readability
-    print FH "unsetenv MP_PE_AFFINITY\n";  # Include this line to avoid caldera problems. CISL-recommended kludge *sigh*
+    if ( $JOBQUEUE =~ "caldera") {
+       print FH "unsetenv MP_PE_AFFINITY\n";  # Include this line to avoid caldera problems. CISL-recommended kludge *sigh*
+    }
     print FH "./geogrid.exe\n";
-    print FH "./link_grib.csh fnl_*\n";
+    print FH "./link_grib.csh grib_*\n";
     print FH "./ungrib.exe\n";
     print FH "./metgrid.exe\n";
     print FH "if ( (!(-e 'met_em.d01.$fcst_end.nc')) && (!(-e '$Script_dir/FAIL')) ) then\n";
@@ -481,7 +482,6 @@ use Getopt::Long;
     print FH "\\cp $Run_dir/$fcst_date/wrfout_d0* $Out_dir/$fcst_dirname/\n";
     close FH ;
 
-
     $job_feedback = ` bsub -w "ended($jobid)" < run_wrf.csh`;
     print "$job_feedback\n";
     if ($job_feedback =~ m/.*<(\d+)>/) {
@@ -490,7 +490,6 @@ use Getopt::Long;
        print "\nJob feedback = $job_feedback\n\n";
        die "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nFailed to submit WRF job for $fcst_date\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
     }
-
 
     chdir $Script_dir;
 
@@ -510,4 +509,86 @@ use Getopt::Long;
       $tm->hour, $tm->min, $tm->sec, $tm->year+1900, $tm->mon+1, $tm->mday;
 
  print "$Finish_time\n";
+
+
+
+
+# SUBROUTINES
+
+ sub getgribfiles {
+
+    my ($date, $fcst_end, $smonth) = @_;
+
+    while ( &wrf2num($date) <= &wrf2num($fcst_end) ) {
+       my $gribyear  = substr("$date", 0, 4);
+       my $gribmonth = substr("$date", 5, 2);
+       my $gribday   = substr("$date", 8, 2);
+       my $gribhour  = substr("$date", 11, 2);
+
+       my @grib_file;
+
+       if ($Data_type eq "FNL") {
+          if ($straddle == 1) {
+             my $monthnumplus = $monthnum + 1;
+             if ($gribmonth == $smonth) {
+                @grib_file = glob("$GRIB_dir[$monthnum]/fnl_$gribyear$gribmonth$gribday\_$gribhour*grib*");
+             } else {
+                @grib_file = glob("$GRIB_dir[$monthnumplus]/fnl_$gribyear$gribmonth$gribday\_$gribhour*grib*");
+             }
+
+          } else {
+             @grib_file = glob("$GRIB_dir[$monthnum]/fnl_$gribyear$gribmonth$gribday\_$gribhour*grib*");
+          }
+       } elsif ($Data_type eq "NAM") {
+          @grib_file = glob("$GRIB_dir[0]/$gribyear$gribmonth$gribday.nam.t${gribhour}*");
+       } else {
+          die "\nBAD DATA TYPE SPECIFIED\n";
+       }
+
+       symlink $grib_file[0], "grib_$gribyear$gribmonth$gribday\_$gribhour" or die "Cannot symlink $_ to local directory: $!\n";
+
+       $date = `$Script_dir/da_advance_time.exe $date ${GRIB_INTERVAL}h -w`;
+       chomp ($date);
+    }
+
+
+
+ }
+
+#Need a subroutine to convert WRF-format dates to numbers for comparison
+ sub wrf2num {
+    my ($wrf_date) = @_;
+    $wrf_date =~ s/\D//g;
+    return $wrf_date;
+ }
+
+ sub current_jobs {
+    my $bjobs = `bjobs`;
+    my $jobnum = () = $bjobs =~ /\d\d:\d\d\n/gi; #This line is complicated; it counts the number of matches for time-like patterns at the end of the line
+                                                 #see http://stackoverflow.com/questions/1849329
+    return $jobnum;
+ }
+
+ sub run_checks { #Subroutine to check for failures and limit simultaneous jobs
+    my $num_jobs =  &current_jobs;
+    if ($num_jobs > $MAX_JOBS) {
+       print "\n\n*********\n* PAUSE *\n*********\n";
+       print "MAX_JOBS exceeded ($num_jobs in queue), waiting before submitting next forecast.\n";
+       printf "We have been waiting for   0 minutes...";
+       my $wait_time = 0;
+       while ($num_jobs > $MAX_JOBS){
+          sleep(60);
+          $wait_time ++;
+          printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b%03d minutes...",$wait_time;
+          $num_jobs =  &current_jobs;
+       }
+    }
+    if (-e "$Script_dir/FAIL") {
+       die "\nFailure reported! See file '$Script_dir/FAIL' for details\n";
+    }
+ }
+
+ sub watch_progress { # When waiting for jobs to finish, let's display some info
+
+}
 
